8410ecd7f05d8ca0223ec7726cc9fffbf6de1f44
florian <florian.cln@gmail.com>
2021-06-02 10:07:33 +0200

    Bootstrap liste-des-citations

--- liste-des-citations.php
+++ liste-des-citations.php
@@ -1,53 +1,35 @@
 <?php
-// Import de la BD
-require "lib/pdo.php";
+require "lib/quote-model.php";
 
-// Création de la connexion
-$cn = getPDO();
-
-// Requête sur la BD
-$sql = "SELECT * FROM citations";
-// -> permet d'exécuter une méthode (equivalent à . sur JS)
-$query = $cn->query($sql);
-
-//Récupération des données
-$data = $query->fetchAll(PDO::FETCH_ASSOC);
-
+$quoteList = getAllQuote();
 
 ?>
 
+<?php require "head.php" ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<body class="container-fluid p-4">
+    <?php require "navigation.php"?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <title>Document</title>
-</head>
+    <div class="rowjustify-content-center">
+        <div class="col-lg-10 p2">
+            <h1>liste des citations</h1>
+            <table class="table table-striped">
+                <tr class="mb-2">
+                    <th>Id</th>
+                    <th>Citation</th>
+                    <th>Auteur</th>
+                </tr>
 
-<body class="container-fluid">
-<?php require "navigation.php"?>
+                <?php foreach($data as $citations): ?>
+                <tr class="justify-content-center">
+                    <td class="col-2"><?= $citations["id"]?></td>
+                    <td class="col-10"><?= $citations["texte"]?></td>
+                    <td class="col-4"><?= $citations["auteur"]?></td>
+                </tr>
+                <?php endforeach?>
+            </table>
 
-    <div class="m-3">
-        <table>
-            <tr class="mb-2">
-                <th>Id</th>
-                <th>Citation</th>
-                <th>Auteur</th>
-            </tr>
-
-            <?php foreach($data as $citations): ?>
-            <tr class="justify-content-center">
-                <td class="table"><?= $citations["id"]?></td>
-                <td class="table"><?= $citations["texte"]?></td>
-                <td class="table"><?= $citations["auteur"]?></td>
-            </tr>
-            <?php endforeach?>
-        </table>
-
+        </div>
     </div>
 
 </body>
